2bc2a75b3dee794f7591e1a63e31858480ddefe7
Alan Tieng <alandtieng@gmail.com>
2024-02-09 14:32:23 -0600

    Update gcodeRotation_octave.m

--- GCode Rotation/gcodeRotation_octave.m
+++ GCode Rotation/gcodeRotation_octave.m
@@ -12,6 +12,21 @@ function foundExp = findExp(vec, i)
     return
 end
 
+% This function transforms coordinates based on a given angle and point of
+% rotation.
+function primeCoord = translate(oldPos, originPrime, theta)
+    %Rotation matrix
+    Q = [cosd(theta), -sind(theta), 0; sind(theta), cosd(theta), 0; 0, 0, 1];
+
+    %Translation matrix
+    T = [1, 0, originPrime(1); 0, 1, originPrime(2); 0, 0, 1];
+
+    %Rotate and find new coordinates
+    oldPos = T*oldPos;
+    primeCoord = (Q)*oldPos;
+    primeCoord = (T^-1)*primeCoord;
+end
+
 %Gcode rotation
 
 %% User input
@@ -22,13 +37,17 @@ originPrime(2) = -input('Point of rotation Y: ');
 readFileName = input('Input filename: ', "s");
 writeFileName = input('Output filename: ', "s");
 
-% Dump text into a single column cell
+%% Dump text into a single column cell
+fid = fopen(readFileName);
+text = textscan(fid, '%s', 'delimiter', ' ');
+fclose(fid);
 
-text = textread(readFileName, '%s', 'delimiter', ' ');
+%% Unpack cell
+text = text{1};
+
+%% Sort and place
 col = 1;
 row = 0;
-
-% Sort and place
 for i = 1:length(text)
   temp = text{i};
   if length(temp)~=0
@@ -45,20 +64,18 @@ for i = 1:length(text)
   endif
 end
 
-%TODO: Clean Up
+%% Divide columns
 G = newText(:,1);
 x = newText(:,2);
 y = newText(:,3);
-if size(newText,2) == 4
-  U = newText(:,4);
-elseif size(newText,2) == 5
-  U = newText(:,4);
-  f = newText(:,5);
-elseif size(newText,2) == 6
-  U = newText(:,4);
-  f = newText(:,5);
-  d = newText(:,6);
-endif
+switch size(newText,2)
+   case 5
+      U = newText(:,4);
+      f = newText(:,5);
+   case 6
+      U = newText(:,4);
+      f = newText(:,5);
+endswitch
 
 %% Calculate new points
 for i = 1:size(G,1)
@@ -69,22 +86,24 @@ for i = 1:size(G,1)
        %Extract position as double from string
        oldPos = [findExp(x,i); findExp(y,i); 1];
 
-       %Rotation matrix
-       Q = [cosd(theta), -sind(theta), 0; sind(theta), cosd(theta), 0;...
-           0, 0, 1];
-
-       %Translation matrix
-       T = [1, 0, originPrime(1); 0, 1, originPrime(2); 0, 0, 1];
-
-       %Rotate and find new coordinates
-       oldPos = T*oldPos;
-       posPrime = (Q)*oldPos;
-       posPrime = (T^-1)*posPrime;
-
+       posPrime = translate(oldPos, originPrime, theta);
        x(i) = ["X" num2str(posPrime(1))];
        y(i) = ["Y" num2str(posPrime(2))];
    end
+   % Translate I and J coordinates if applicable
+   if (strcmp(G(i), "G2") || strcmp(G(i), "G3") || strcmp(G(i), "G02")...
+           || strcmp(G(i), "G03")) && (size(newText,2) > 4)
+      ch = U{i};
+      cy = f{i};
+      if ch(1) == "I" && cy(i) == "J"
+         %Extract position as double from string
+         oldPos = [findExp(U,i); findExp(f,i); 1];
 
+         posPrime = translate(oldPos, originPrime, theta);
+         U(i) = ["I" num2str(posPrime(1))];
+         f(i) = ["J" num2str(posPrime(2))];
+      endif
+   end
 end
 
 newText(:,2) = x(:);
