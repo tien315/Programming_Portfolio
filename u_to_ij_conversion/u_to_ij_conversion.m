ce554699f8e6342b9e981d6a504e4d0a4490a5af
Alan Tieng <alandtieng@gmail.com>
2024-02-13 15:51:55 -0600

    Update u_to_ij_conversion.m

--- u_to_ij_conversion/u_to_ij_conversion.m
+++ u_to_ij_conversion/u_to_ij_conversion.m
@@ -2,13 +2,11 @@ close all;
 clear all;
 clc;
 
-%% User input
-
+% User input
 readFileName = input('Input filename: ', "s");
 writeFileName = input('Output filename: ', "s");
 
-%% Parse Text and calculate new center points
-
+% Parse Text and calculate new center points
 fid = fopen(readFileName);
 text = textscan(fid, '%s%s%s%s%s', 'Delimiter', ' ', ...
     'MultipleDelimsAsOne', true, 'TextType', 'string', ...
@@ -22,7 +20,7 @@ U = text{4};
 f = text{5};
 
 for i = 1:size(G,1)
-   if (G{i} == "G2") || (G{i} == "G3")
+   if (isMovement(G{i}, "circular movement"))
        
        %find the start coordinates
        mIndex = i-1;
@@ -37,7 +35,7 @@ for i = 1:size(G,1)
        endCoord = findCoord(x,y,i); %[x,y]
        newCenter = findNewCenter(startCoord, endCoord, findExp(U, i));
 
-       if (G{i} == "G2")
+       if (isMovement(G{i}, "CCW"))
            U{i} = convertStringsToChars("I"+num2str(newCenter(1,1)));
            f{i} = convertStringsToChars("J"+num2str(newCenter(1,2)));
        else
@@ -49,33 +47,68 @@ end
 
 rawToFile = [G,x,y,U,f]';
 
-%% Write to file
+% Write to file
 fid = fopen(writeFileName, 'w');
 fprintf(fid, '%s %s %s %s %s\n', rawToFile);
 fclose(fid);
 
-%% Functions 
+% Functions 
 
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
 function [coord] = findCoord(x,y,i)
     coord(1) = findExp(x, i);
     coord(2) = findExp(y, i);
     return
 end
 
+%--------------------------------------------------------------------------
+% This function checks if the string is a movement command.
+% Inputs:
+% gString - string
+% checkType - string, identifies call for movement or circular movement
+%             check.
+% Outputs:
+% movement - bool
+%--------------------------------------------------------------------------
+function movement = isMovement(gString, checkType)
+  switch checkType
+      case "CCW"
+          checkList = ["G02" "G2"];
+      case "circular movement"
+          checkList = ["G02" "G03" "G2" "G3"];
+  end
+  movement = false;
+  for i = 1:length(checkList)
+    if strcmp(checkList(i), gString)
+      movement = true;
+      break;
+    end
+  end
+end
+
+%--------------------------------------------------------------------------
 % Numbers in the dataset are stored as a string combined with letters (ex.
 % X101.2568 or U-0.303).  This function separates the number and converts 
 % it into a double as the output.
+%--------------------------------------------------------------------------
 function foundExp = findExp(vec, i)
-    foundExp = str2double(regexp(vec{i},'[\d.\-]+','match'));
+    foundExp = str2double(regexp(vec{i},'[\d.]+','match'));
     return
 end
 
+%--------------------------------------------------------------------------
 % This function calculates the intersections of two equal radius circles.
-% Output newCenter = [G2 x, G2 y; G3 x, G3 y]
+% Output is in the form: newCenter = [G2 x, G2 y; G3 x, G3 y]
+% Input:
+% startCoord - double (1 x 2)
+% endCoord - double (1 x 2)
+% U - double
+% iteration - int
+% Output:
+% newCenter - double (2 x 1)
+%--------------------------------------------------------------------------
 function [newCenter] = findNewCenter(startCoord, endCoord, U, iteration)
-
-disp(iteration)
-
     % Find distance between start and end points
     d = sqrt((endCoord(1)-startCoord(1))^2+(endCoord(2)-startCoord(2))^2);
     
@@ -92,17 +125,17 @@ disp(iteration)
             num2str(iteration)])
         return
     end
-
+    
     x = d/2;
     y = sqrt(U^2-(d/2)^2);
     theta = atand((endCoord(2)-startCoord(2))/(endCoord(1)-startCoord(1)));
-
+    
     if (endCoord(1) < startCoord(1))
         theta = theta + 180;
     end
-
+    
     % Tranformation matrix
-    Q = [cosd(theta), -sind(theta); sind(theta), cosd(theta)];
+    Q = [cosd(theta), -sind(theta); sind(theta), cosd(theta)];  
     
     % U values < 0 indicate an arc tracing > 180 degrees so we need to flip
     % the center used.
@@ -114,7 +147,6 @@ disp(iteration)
         center2 = Q*[x;-y];
     end
     
-    newCenter = round([center2'; center1'], 3);    
-
+    newCenter = round([center2'; center1'], 3);        
     return
 end
